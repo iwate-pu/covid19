e6aacbe3534e7cbc71cb019bcf03d13525501d74
Issei Komatsu <issei@ikmz.net>
2020-03-26 01:42:55 +0900

    修正

--- components/TopBarNotice.vue
+++ components/TopBarNotice.vue
@@ -3,13 +3,15 @@
     このサイトはメンテナンスされていません。最新情報は
     <a
       href="https://www.pref.iwate.jp/kurashikankyou/iryou/kenkou/jouhou/1026260.html"
+      target="_blank"
+      rel="noopener noreferrer"
     >新型コロナウイルス感染症に関する情報(岩手県)</a>
     または
     <a
       href="https://covid19-iwate.netlify.com/"
       target="_blank"
       rel="noopener noreferrer"
-    >岩手県　非公式　コロナウイルス感染症対策サイト</a>
+    >岩手県非公式コロナウイルス感染症対策サイト</a>
     をご覧ください。
   </div>
 </template>
